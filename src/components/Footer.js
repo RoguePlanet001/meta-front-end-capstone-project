4a48ebd68108c31d05b5d0d998259c073d1af3ea
Matthew <matthew_webdev@yahoo.com>
2025-06-17 20:07:40 -0500

    Added <Footer/> component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 export default function Footer () {
     return (
         <footer>
-            I haven't seen any instructions on what the footer should look like.
+            <address>
+                Little Lemon<br/>
+                123 Main Street<br/>
+                Chicago, IL 98765
+            </address>
+            <div>
+                <a href='tel:5550123456'>555-012-3456</a>
+                <a href='mailto:customer_service@littlelemon.com'>customer_service@littlelemon.com</a>
+            </div>
+            <p>&copy; Little Lemon, LLC</p>
+
         </footer>
     );
 }
